acf1da5bcb5ba42e50b2d9a5fbe962331d4403e3
chrisarap <carteaga460@gmail.com>
2022-06-12 18:10:48 -0400

    how to put a profile together

--- server.js
+++ server.js
@@ -48,7 +48,10 @@ myDB(async client => {
   app.route('/profile').get(
     ensureAuthenticated,
     (req, res) => {
-      res.render(process.cwd() + '/views/pug/profile');
+      res.render(
+        process.cwd() + '/views/pug/profile',
+        {username: req.user.username}
+      );
     }
   );
 
